47f5cf9d8e6d44bd6f150555d937d4eb68d26219
emmarousseau <emmarou1@icloud.com>
2024-09-15 11:51:13 +0200

    Revert "Merge branch 'main' into kallisto_index"
    This reverts commit f6c2cdc2c0d616da613cd6eef2b446db71891d99, reversing
    changes made to f75d6db97e78792d3d9e7fd3299654abf6327050.

--- src/sortmerna/test_data/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -d /tmp/sortmerna_source ]; then
-  git clone --depth 2 --single-branch --branch master https://github.com/snakemake/snakemake-wrappers.git /tmp/sortmerna_source
-fi
-
-# copy test data
-cp -r /tmp/sortmerna_source/bio/sortmerna/test/* .
